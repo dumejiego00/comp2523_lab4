67a5548b315884491a69acad85ebea445388b0ba
Daniel Umejiego <danielumejiego@yahoo.com>
2024-01-30 11:44:40 -0800

    final check done

--- test.ts
+++ test.ts
@@ -2,6 +2,7 @@ import { CharacterGroup } from "./CharacterGroup";
 import { LinkedListGroup } from "./LinkedListGroup";
 import { NumberGroup } from "./NumberGroup";
 import { SortUtil } from "./SortUtil";
+
 const numberGroup = new NumberGroup([30, 3, -15, 0]);
 const sorter1 = new SortUtil(numberGroup);
 sorter1.sort();
