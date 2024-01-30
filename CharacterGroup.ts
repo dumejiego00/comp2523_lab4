1a1bafcab226057de5879c28531e8d45e224570d
Daniel Umejiego <danielumejiego@yahoo.com>
2024-01-30 10:53:06 -0800

    CharacterGropu done working on LinkedListGroup

--- CharacterGroup.ts
+++ CharacterGroup.ts
@@ -1,15 +1,21 @@
-export class CharacterGroup {
-    data: string;
-    constructor(string:string){
-        this.data = string;
-    }
+import { ISortable } from "./ISortable";
+
+export class CharacterGroup implements ISortable {
+  data: string;
+  constructor(string: string) {
+    this.data = string;
+  }
   get length(): number {
     return this.data.length;
   }
-  swap(leftPos: number, rightPos: number):void {
-    return
+  swap(leftPos: number, rightPos: number): void {
+    let stringArray = this.data.split("");
+    const temp = stringArray[leftPos];
+    stringArray[leftPos] = stringArray[rightPos];
+    stringArray[rightPos] = temp;
+    this.data = stringArray.join("");
   }
   compare(leftPos: number, rightPos: number): boolean {
-    return true;
+    return this.data[leftPos].toLowerCase() > this.data[rightPos].toLowerCase();
   }
-  }
+}
--- test.ts
+++ test.ts
@@ -1,7 +1,14 @@
+import { CharacterGroup } from "./CharacterGroup";
 import { NumberGroup } from "./NumberGroup";
 import { SortUtil } from "./SortUtil";
 const numberGroup = new NumberGroup([30, 3, -15, 0]);
 const sorter1 = new SortUtil(numberGroup);
 sorter1.sort();
 console.log(numberGroup.data);
+console.log("---------------------------");
+
+const characterGroup = new CharacterGroup("Xaeyb");
+const sorter2 = new SortUtil(characterGroup);
+sorter2.sort();
+console.log(characterGroup.data);
 console.log("---------------------------");
